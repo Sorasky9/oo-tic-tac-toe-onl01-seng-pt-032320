d18e6f3b17296c65007537eb2a42a18a82eec074
Malcom Fetus <sora.vista@gmail.com>
2020-04-26 00:30:46 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,34 @@
+require 'pry'
+class TicTacToe
+
+WIN_COMBINATIONS = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[6,4,2],
+]
+
+def initialize(board = Array.new(9, " "))
+  @board = board
+end
+
+def display_board
+puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+puts "-----------"
+puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+puts "-----------"
+puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def input_to_index(input)# the input argument will receive the number on the board being chosen to put their token ("X", or "O")
+input.to_i-1 #convert the input to integer (which give 1) here and minus one to match the indexes on the board
+end
+
+def move(board = [0], token = "X")
+end
+
+end
